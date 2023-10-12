9dd18ec1bb248f7de1b697493112fc779228a8c8
pemtshewang <pemtshewang500@gmail.com>
2023-10-12 22:42:34 +0600

    added new files

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View>
+        <Text style={styles.text}>{count}</Text>
+        <Button onPress={
+          () => {
+            setCount(prevState => prevState + 1);
+          }
+        } title='Click me' />
+      </View>
     </View>
   );
 }
@@ -13,8 +23,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    fontSize: 23,
+    borderColor: '#000000',
+    borderWidth: 4,
+    textAlign: "center",
+    marginBottom: 4
+  }
 });
